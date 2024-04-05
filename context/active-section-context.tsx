f048db43e6113cc4cbe8003936bd86bcecf4df0a
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2024-04-05 19:50:26 +0200

    active section added , header edited

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
-import { links } from '@/lib/data';
+"use client";
 
-type SectionName = (typeof links)[number]["name"];
+import type { SectionName } from "@/lib/types";
+import React, { useState, createContext, useContext } from "react";
 
 type ActiveSectionContextProviderProps = {
-    children : React.ReactNode;
-}
+  children: React.ReactNode;
+};
+
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+
+};
+
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
 
 export default function ActiveSectionContextProvider({
-    children,
+  children,
 }: ActiveSectionContextProviderProps) {
-    const [activeSection, setActiveSection] = useState<SectionName>("Home");
-    
-    return children;
+  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within an ActiveSectionContextProvider"
+    );
+  }
+
+  return context;
 }
