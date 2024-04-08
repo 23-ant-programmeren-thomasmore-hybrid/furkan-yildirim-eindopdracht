99aefd846c32acecb99c800c9e8cba93aa4e8c87
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2024-04-08 06:13:12 +0200

    footer added

--- .history/app/page_20240408061059.tsx
+++ .history/app/page_20240408061059.tsx
@@ -5,7 +5,6 @@ import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
-import Footer from "@/components/footer";
 
 
 
@@ -19,7 +18,7 @@ export default function Home() {
       <Skills/>
       <Experience/>
       <Contact/>
-      <Footer/>
+      
     </main>
   )
 }
